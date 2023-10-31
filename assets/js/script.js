3d4ce364568a633be3f1706f189d557c44458840
Romina Alejandra Barrionuevo <robarrionuevo13@gmail.com>
2023-10-31 14:04:45 -0300

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,17 @@
 
- 
+
+
+
+
+ function suma() {
+
+    let cantidad = document.getElementById('cantidad').value;
+    let categoria = document.getElementById('categoria').value;
+    let estudiante = 200 - 200 * 0.80;
+    let general = 200;
+    let Trainee = 200 - 200 * 0.50;
+    let Junior = 200 - 200 * 0.15;
+
+
+
+ }
